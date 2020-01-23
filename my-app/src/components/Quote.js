02537736222ae2b0009fd7da5468f7c323df547b
Devanee Reid <devaneereid@gmail.com>
2020-01-23 15:39:20 -0700

    added some styling

--- my-app/src/components/Quote.js
+++ my-app/src/components/Quote.js
@@ -2,22 +2,54 @@ import React from 'react';
 import {connect} from 'react-redux';
 import Loader from 'react-loader-spinner';
 import {getQuote} from '../actions';
+import styled from 'styled-components';
+
+const DivStyles = styled.div`
+    padding: 30px;
+    border: 2px solid lavender;
+    border-radius: 20px;
+    margin: 20px 40px;
+    background: seashell;
+`;
+
+const H1Styles = styled.h1`
+    font-family: monospace;
+    font-size: 2.2rem;
+    font-style: oblique;
+`;
+
+const QuoteStyles = styled.h5`
+    font-family: monospace;
+    font-size: 1rem;
+    padding: 10px;
+    letter-spacing: 1.2px;
+`;
+
+const Button = styled.button`
+    background: dimgray;
+    color: white;
+    padding: 15px 40px;
+    border-radius: 20px;
+    font-family: monospace;
+    margin: 20px;
+    letter-spacing: 1.2px;
+`;
 
 const Quote = props => {
     return (
-        <div>
-            <h1>Ron Swanson Knows Best!</h1>
+        <DivStyles>
+            <H1Styles>Ron Swanson Knows Best!</H1Styles>
             {props.isFetching && (
                 <Loader 
-                    type='Puff' 
+                    type='Ball-Triangle' 
                     color='aqua' 
                     height={100} 
                     width={100} 
-                    timeout={4000} />
+                />
             )}
-            {props.data && <h5>{props.data.data}</h5>}
-            <button onClick={props.getQuote}>Wisdom from Ron</button>
-        </div>
+            {props.data && <QuoteStyles>{props.data.data}</QuoteStyles>}
+            <Button onClick={props.getQuote}>Wisdom from Ron</Button>
+        </DivStyles>
     );
 };
 
--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -34,4 +34,5 @@ const reducer = (state = initialState, action) => {
             return state;
     }
 };
+
 export default reducer;
